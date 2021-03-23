aac92f7af54b7eb498089f0920aaffd77e638589
ilgabrie <36480642+ilgabrie@users.noreply.github.com>
2021-03-23 12:59:51 +0000

    Update start_dss.sh

--- image/start_dss.sh
+++ image/start_dss.sh
@@ -4,7 +4,7 @@ DSS_INSTALLDIR="/home/bluedata/dataiku-dss-$DSS_VERSION"
 
 if [ ! -f "$DSS_DATADIR"/bin/env-default.sh ]; then
 	# Initialize new data directory
-	"$DSS_INSTALLDIR"/installer.sh -d "$DSS_DATADIR" -p "$DSS_PORT"
+	"$DSS_INSTALLDIR"/installer.sh -d "$DSS_DATADIR" -p "$DSS_PORT" -l /home/bluedata/license.json
 	"$DSS_DATADIR"/bin/dssadmin install-R-integration
 	echo "dku.registration.channel=docker-image" >>"$DSS_DATADIR"/config/dip.properties
 
